a82e64549f5fa9a274fc84b265b3a477d54fc18d
Felipe Alves <profelipealves@outlook.com>
2023-05-22 18:21:28 -0300

    ✨ add login github and image logo

---
+++ spacetime-reactnative/app/index.tsx
@@ -0,0 +1,56 @@
+import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import * as SecureStore from 'expo-secure-store'
+
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/d26f194cc5d5132a51be',
+}
+
+export default function App() {
+  const router = useRouter()
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: 'd26f194cc5d5132a51be',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
+
+  return (
+    <View className='flex-1 items-center px-8 py-10'>
+      <View className='flex-1 items-center justify-center gap-6'>
+        <NLWLogo />
+      </View>
+    </View>
+  )
+}
