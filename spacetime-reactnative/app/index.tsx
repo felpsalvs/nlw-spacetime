9b8a51075fdb9c0276c37495143cc0a5233998e8
Felipe Alves <profelipealves@outlook.com>
2023-05-23 15:54:12 -0300

    ✨ add styles in UI

--- spacetime-reactnative/app/index.tsx
+++ spacetime-reactnative/app/index.tsx
@@ -10,7 +10,8 @@ import { api } from '../src/lib/api'
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
   tokenEndpoint: 'https://github.com/login/oauth/access_token',
-  revocationEndpoint: 'https://github.com/settings/connections/applications/d26f194cc5d5132a51be',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/d26f194cc5d5132a51be',
 }
 
 export default function App() {
@@ -47,10 +48,34 @@ export default function App() {
   }, [response])
 
   return (
-    <View className='flex-1 items-center px-8 py-10'>
-      <View className='flex-1 items-center justify-center gap-6'>
+    <View className="flex-1 items-center px-8 py-10">
+      <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilher com o
+            mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
       </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
     </View>
   )
 }
